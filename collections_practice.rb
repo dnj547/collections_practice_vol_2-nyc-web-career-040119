610990397f66d26023cfd216af0e10bd3429aefe
Danielle Jasper <dnj547@live.com>
2019-03-16 14:17:48 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,53 @@
-# your code goes here
+require 'pry'
+def begins_with_r(tools)
+  tools.all? {|tool| tool.match(/^r/)}
+end
+
+def contain_a(array)
+  array.select {|element| element.include? "a"}
+end
+
+def first_wa(array)
+  array.find {|element| element.match(/^wa/)}
+end
+
+def remove_non_strings(array)
+  array.reject {|element| !element.is_a? String}
+end
+
+def count_elements(array)
+  array.each do |hash|
+    hash[:count] = 0
+    array.each do |new_hash|
+      if new_hash[:name] == hash[:name]
+        hash[:count] += 1
+      end
+    end
+  end.uniq
+end
+
+def merge_data(arr1, arr2)
+  merged_hash = {}
+  arr1.each do |keys_hash|
+    keys_hash.each do |key1, value1|
+      merged_hash[key1] = value1
+      arr2.each do |data_hash|
+        data_hash.each do |name, hash3|
+          if value1 == name
+            hash3.each do |key3, value3|
+              merged_hash[key3] = value3
+              binding.pry
+            end
+          end
+        end
+      end
+    end
+  end
+  return merged_hash
+end
+
+def find_cool
+end
+
+def organize_schools
+end
