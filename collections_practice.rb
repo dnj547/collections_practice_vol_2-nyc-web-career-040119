3348612ec9e78b485e2ad3645310150bfeaa8715
Danielle Jasper <dnj547@live.com>
2019-03-16 15:05:07 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -38,7 +38,6 @@ def merge_data(arr1, arr2)
               hash.each do |key1, value1|
                 new_hash[key1] = value1
               end
-              binding.pry
               new_array << new_hash
               return new_array
             end
